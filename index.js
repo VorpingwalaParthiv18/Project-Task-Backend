80def0d0380e02beb86cf724160cd6aaa6ad9a4e
VorpingwalaParthiv18 <vorpingwalaparthiv@gmail.com>
2025-10-16 18:44:04 +0530

    changes index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"], // allows requests from any origin
+    origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175","https://project-task-backend-pdu2.vercel.app"], // allows requests from any origin
     credentials: true, // allow cookies to be sent
   })
 );
