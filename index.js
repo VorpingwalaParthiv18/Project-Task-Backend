8980398c09120c162334ea07bacb150b63201151
VorpingwalaParthiv18 <vorpingwalaparthiv@gmail.com>
2025-10-17 10:56:21 +0530

    changes idex file

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"]
+    origin: "http://localhost:5173",
+    credentials: true 
   })
 );
 
